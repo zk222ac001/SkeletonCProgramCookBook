1e6d131bd97befad40d5d1e33595504b538598cd
Zuhair Haroon Khan <zk222ac@gmail.com>
2024-09-24 16:08:59 +0200

    V1.0

--- HowToRunFirstProgramInCHelloWorld/HelloWorld.cpp
+++ HowToRunFirstProgramInCHelloWorld/HelloWorld.cpp
@@ -1,8 +1,9 @@
+// V1.0
 // DDL --> dynamic link library
 // stdio --> standard input out library contains two main functions (printf and scanf)
 #include <stdio.h>
 
 int main(void) {
-	// Print hello world information on the screen
+	// Print hello world information on the screen	
 	printf("Hello world");
 }
