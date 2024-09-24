ffe54d5251f01ef7a0f29bb6171d4778e5c7519f
Zuhair Haroon Khan <zk222ac@gmail.com>
2024-09-24 19:28:02 +0200

    Add Project Escape characters

---
+++ EscapeCharactersInC/EscapeCharactersProject.cpp
@@ -0,0 +1,19 @@
+// V1.0
+// DDL --> dynamic link library
+// stdio --> standard input out library contains two main functions (printf and scanf)
+#include <stdio.h>
+
+int main(void) {
+	// Print hello world information on the screen	
+	// escape characters \n --> move to the next line 
+	//                    \t --> horizontal tab 
+	//                    \v ---> vertical tab
+	printf("Hello\t world\n");
+	printf("Hello\t world\n");
+	printf("Hello\t world\n");
+	printf("Hello\t world\n");
+	printf("Hello\t world\n");
+	printf("Hello\t world\n");
+	printf("Hello\t world\n");
+	printf("Hello\t world\n");
+}
