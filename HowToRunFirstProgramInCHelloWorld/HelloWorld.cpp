832f1f384b1923f3391217e03e60736a48fa2461
ZUKH <ZUKH@Z4092>
2024-09-24 16:06:47 +0200

    Add project files.

---
+++ HowToRunFirstProgramInCHelloWorld/HelloWorld.cpp
@@ -0,0 +1,8 @@
+// DDL --> dynamic link library
+// stdio --> standard input out library contains two main functions (printf and scanf)
+#include <stdio.h>
+
+int main(void) {
+	// Print hello world information on the screen
+	printf("Hello world");
+}
